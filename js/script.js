d075f80e703d641c73f55bdc1bcf3400b3e0104e
Anushree Bagchi <anushreebagchi4791@gmail.com>
2019-01-04 19:40:13 +0530

    Inbox mail content added

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
 var inbox=document.getElementById('inbox');
+var subject= document.getElementById('subject');
+
+function onClickInbox(){
+    fetch('code/inbox.json')
+    .then(function(response) {
+    return response.json();
+    })
+    .then(data => {
+    console.log( data );
+    for (var i =0; i< data.length;i++){
+    $('#mail').append(`<p class='subject'> ${data[i].subject} </p> `);
+    $('#mail').append(`<p class='mail-content'>${data[i].content}</p><br>`);
+    }
+    });
+}
 
 inbox.addEventListener('click',function (){
     console.log('clicked');
+    onClickInbox();
 });
-var inbox_data=[];
-
-fetch('code/inbox.json')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(data => {
-    console.log( data[0] );
-  });
-
-// console.log(inbox_data);
