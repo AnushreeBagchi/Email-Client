5a54d0907fcfef59af349952fe7eb3f1f90098ee
Anushree Bagchi <anushreebagchi4791@gmail.com>
2019-01-05 12:02:06 +0530

    Changes commited

--- js/script.js
+++ js/script.js
@@ -1,21 +1,66 @@
-var inbox=document.getElementById('inbox');
-var subject= document.getElementById('subject');
+var inbox = document.getElementById('inbox');
+var junk = document.getElementById('junk');
+var sent = document.getElementById('sent');
+// var junk = document.getElementById('junk');
+// var junk = document.getElementById('junk');
+var url;
 
-function onClickInbox(){
-    fetch('code/inbox.json')
-    .then(function(response) {
-    return response.json();
-    })
-    .then(data => {
-    console.log( data );
-    for (var i =0; i< data.length;i++){
-    $('#mail').append(`<p class='subject'> ${data[i].subject} </p> `);
-    $('#mail').append(`<p class='mail-content'>${data[i].content}</p><br>`);
-    }
+function onClick(url) {
+
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(data => {
+            $('.message-list').empty(); // clear exiting data 
+            for (var i = 0; i < data.length; i++) {
+                $('.message-list').append(`<div class ='mail'><p class='subject'> ${data[i].subject} </p> 
+                <p class='mail-content'>${data[i].content}</p><br></div>`);
+            };
+
+            // on click mail
+            onMailClick();
+        });
+};
+
+function onMailClick(){
+    $('.default-content').removeClass('hide');
+    $('.message-body').empty();
+    $('.mail').on('click',function(){
+        // console.log();
+        $('.default-content').addClass('hide');
+        $('.message-body').empty();
+        // debugger;
+        $('.message-body').append($(this).children('.mail-content').text());
     });
 }
 
-inbox.addEventListener('click',function (){
-    console.log('clicked');
-    onClickInbox();
+function bold(obj){
+    $('.folder').css('font-weight','normal');
+    obj.css('font-weight', 'bold');
+};
+
+// Inbox
+
+inbox.addEventListener('click', function () {
+    url = 'code/inbox.json';
+    bold($(this));
+    onClick(url);
+});
+
+// Junk items
+
+junk.addEventListener('click', function () {
+    url = 'code/spam.json';
+    bold($(this));
+    onClick(url);
+
+});
+
+//sent items
+sent.addEventListener('click', function () {
+    url = 'code/sent.json';
+    bold($(this));
+    onClick(url);
+
 });
